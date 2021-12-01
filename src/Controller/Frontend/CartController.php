d603984d40e53e77bad1afd6c81579c78e4d13c7
REAUME Fabrice <fabrice.reaume-77@outlook.fr>
2021-12-01 14:09:54 +0100

    cart update

--- src/Controller/Frontend/CartController.php
+++ src/Controller/Frontend/CartController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller\Frontend;
 
-use App\Class\Cart;
+
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
@@ -14,40 +15,91 @@ class CartController extends AbstractController
     /**
      * @Route("/panier", name="app_frontend_cart_index")
      */
-    public function index(Cart $cart, ProductRepository $productRepository): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
-        $total = [];
+        //Retrieving the current cart
+        $cart = $session->get("cart", []);
 
-        foreach($cart->get() as $id =>$quantity)
+        //Data manufacturing
+        $dataCart = [];
+        $total = 0;
+
+        foreach($cart as $id => $quantity)
         {
-            $total[] = [
-                'product' => $productRepository->findOneById($id),
-                'quantity' => $quantity
+            $product = $productRepository->find($id);
+            $dataCart[] = [
+                "product" => $product,
+                "quantity" => $quantity
             ];
+            $total += $product->getPrice() * $quantity;
         }
 
-        return $this->render('frontend/cart/index.html.twig', [
-            'cart' => $total
-        ]);
+        return $this->render('frontend/cart/index.html.twig', compact("dataCart", "total"));
     }
 
     /**
      * @Route("/cart/add/{id}", name="app_frontend_cart_add")
      */
-    public function add(Cart $cart, $id): Response
+    public function add(Product $product, SessionInterface $session): Response
     {
-        $cart->add($id);
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+
+        if(!empty($cart[$id]))
+        {
+            $cart[$id]++;
+        }
+        else
+        {
+            $cart[$id] = 1;
+        }
+
+        //Backup in the session
+        $session->set("cart", $cart);
 
         return $this->redirectToRoute('app_frontend_cart_index');
     }
 
     /**
-     * @Route("/cart/remove", name="app_frontend_cart_remove")
+     * @Route("/cart/remove/{id}", name="app_frontend_cart_remove")
      */
-    public function remove(Cart $cart): Response
+    public function remove(Product $product, SessionInterface $session): Response
     {
-        $cart->remove();
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
 
-        return $this->redirectToRoute('app_frontend_product_index');
+        if(!empty($cart[$id]))
+        {
+            if($cart[$id] > 1)
+            {
+                $cart[$id]--;
+            }
+            else
+            {
+                unset($cart[$id]);
+            }
+        }
+
+        $session->set("cart", $cart);
+
+        return $this->redirectToRoute('app_frontend_cart_index');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="app_frontend_cart_delete")
+     */
+    public function delete(Product $product, SessionInterface $session): Response
+    {
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+
+        if(!empty($cart[$id]))
+        {
+                unset($cart[$id]);
+        }
+
+        $session->set("cart", $cart);
+
+        return $this->redirectToRoute('app_frontend_cart_index');
     }
 }
